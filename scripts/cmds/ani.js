a7ece9a8f8874227c13c407ad2df94c083449c0f
botgit69 <githubtermux8@gmail.com>
2025-10-27 21:34:57 +0300

    Create ani.js

---
+++ scripts/cmds/ani.js
@@ -0,0 +1,79 @@
+cmd install ani.js const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+async function getStreamFromURL(url) {
+  const response = await axios.get(url, { responseType: 'stream' });
+  return response.data;
+}
+
+async function fetchTikTokVideos(query) {
+  try {
+    const response = await axios.get(`https://lyric-search-neon.vercel.app/kshitiz?keyword=${query}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+module.exports = {
+  config: {
+    name: "ani",
+    aliases: ["rm","anime"],
+    author: "Rocky", // ❌ Do not change this
+    version: "1.0",
+    shortDescription: {
+      en: "get anime edit",
+    },
+    longDescription: {
+      en: "search for anime edits video",
+    },
+    category: "fun",
+    guide: {
+      en: "{p}{n} [query]",
+    },
+  },
+
+  // 🔒 Author protection
+  onLoad: function () {
+    if (this.config.author !== "Rocky") {
+      throw new Error("❌ Do not change the author name! File stopped.");
+    }
+  },
+
+  onStart: async function ({ api, event, args }) {
+    api.setMessageReaction("❓", event.messageID, (err) => {}, true);
+
+    const query = args.join(' ');
+    const modifiedQuery = `${query} anime edit`;
+
+    const videos = await fetchTikTokVideos(modifiedQuery);
+
+    if (!videos || videos.length === 0) {
+      api.sendMessage({ body: `${query || "Anime edit"} not found.` }, event.threadID, event.messageID);
+      return;
+    }
+
+    const selectedVideo = videos[Math.floor(Math.random() * videos.length)];
+    const videoUrl = selectedVideo.videoUrl;
+
+    if (!videoUrl) {
+      api.sendMessage({ body: 'Error: Video not found.' }, event.threadID, event.messageID);
+      return;
+    }
+
+    try {
+      const videoStream = await getStreamFromURL(videoUrl);
+
+      await api.sendMessage({
+        body: ``,
+        attachment: videoStream,
+      }, event.threadID, event.messageID);
+    } catch (error) {
+      console.error(error);
+      api.sendMessage({ body: 'An error occurred while processing the video.\nPlease try again later.' }, event.threadID, event.messageID);
+    }
+  },
+};
